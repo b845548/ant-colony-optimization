6741f72836cde4d1fa6e5f90ca2125f0bb44889b
HBB <b845548@gmail.com>
2017-04-08 13:00:32 -0400

    tt

--- fourmis.c
+++ fourmis.c
@@ -15,9 +15,6 @@
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/property_map/property_map.hpp>
 
-// Create a struct to hold properties for each vertex
-//typedef int fourmi;
-
 typedef struct VertexProperties
 {
     mutable int passage;
@@ -26,7 +23,6 @@ typedef struct VertexProperties
 
 } VertexProperties;
 
-// Create a struct to hold properties for each edge
 typedef struct EdgeProperties
 {
     mutable double pheromones;
@@ -69,69 +65,79 @@ void random_graph(Graph& g, int nb_vertex, int perc) {
 }
 
 
-void affichage(const Graph& g) {
-/*
-    vector<int> vertex(num_vertices(g));
-
-    std::generate_n(vertex.begin(), vertex.size(), []() -> int { static int i = 0; return i++; });
-    std::sort(vertex.begin(), vertex.end(),
-              [&g](auto a, auto b) { return out_degree(a, g) > out_degree(b, g); }
-              );
-*/
-    for(auto ve = vertices(g);ve.first!=ve.second;++ve.first) {
-         std::cout<< g[*ve.first].ville << " nbf: "<< g[*ve.first].nb_fourmis<<std::endl;
-         for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) {
+void affichage(const Graph& g) 
+{
+    for(auto ve = vertices(g);ve.first!=ve.second;++ve.first) 
+    {
+        std::cout<< g[*ve.first].ville << " nbf: "<< g[*ve.first].nb_fourmis<<std::endl;
+        for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) 
+        {
             std::pair<Graph::edge_descriptor, bool> edgeProperty = boost::edge(*ve.first, *pair_it.first, g);
             std::cout<<*ve.first<<"->"<< *pair_it.first<<" d: "<<g[edgeProperty.first].distance <<" p: "<< g[edgeProperty.first].pheromones << std::endl;
         }
     }
-    return;
 }
 
 
-void deplacement(const Graph& g) {
-  double probabiliteMax,probabilite;
-  int indexMax;
-    for(auto ve = vertices(g);ve.first!=ve.second;++ve.first) {
-        while(g[*ve.first].nb_fourmis!=0){
+void deplacement(const Graph& g) 
+{
+    double probabiliteMax,probabilite;
+    int indexMax;
+
+    // POUR TOUS LES SOMMETS
+    for(auto ve = vertices(g);ve.first!=ve.second;++ve.first) 
+    {
+
+        // TANT QUE IL Y A UN FOURMIS A DEPLACER
+        while(g[*ve.first].nb_fourmis!=0)
+        {
+
+            // TROUVER MAX PROBABILITE
             indexMax=-1;
             probabiliteMax=-1.0;            
-            for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) {         
+            for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) 
+            {         
                 std::pair<Graph::edge_descriptor, bool> edgeProperty = boost::edge(*ve.first, *pair_it.first, g);
                 probabilite=g[edgeProperty.first].pheromones/(double)g[edgeProperty.first].distance;
-                if(probabiliteMax < probabilite){
+                if(probabiliteMax < probabilite)
+                {
                     probabiliteMax = probabilite;
                     indexMax = *pair_it.first;
                 }
             }
+            // TROUVER MAX PROBABILITE
+
+            // DEPLACEMENT D'UN FOURMIS 
             g[*ve.first].nb_fourmis--;
             g[indexMax].nb_fourmis++;
             g[indexMax].passage++;
-            
-            for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) {         
+            // DEPLACEMENT D'UN FOURMIS
+
+            // UPDATE PHEROMONE
+            for (auto pair_it = adjacent_vertices(*ve.first, g); pair_it.first != pair_it.second; ++pair_it.first) 
+            {         
                 std::pair<Graph::edge_descriptor, bool> edgeProperty = boost::edge(*ve.first, *pair_it.first, g);
-                g[edgeProperty.first].pheromones=g[edgeProperty.first].pheromones/2. + 100 * g[*ve.first].passage;
+                g[edgeProperty.first].pheromones=g[edgeProperty.first].pheromones/2. + 100. * g[*ve.first].passage;
             }
+            // UPDATE PHEROMONE
 
         }
+        // TANT QUE IL Y A UN FOURMIS A DEPLACER
+
     }
-    return;
+    // POUR TOUS LES SOMMETS
 }
 
 
-
-
+// g++ test.c -I/home/hbxxx/boost_1_63_0 -std=c++11
 int main(int,char*[])
 {
-  // Create a graph object
   Graph g;
   random_graph(g, 40, 3);
   affichage(g);
-  deplacement(g);
+  for(int i=0;i<1000;++i)
+      deplacement(g);
   std::cout << "num_vertices: " << boost::num_vertices(g) << std::endl;
   std::cout << "num_edges: " << boost::num_edges(g) << std::endl;
-
-  // compute ST shortest paths here...
-
   return 0;
 }
